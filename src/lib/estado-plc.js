5fbb50ef4965a76de6fd9ce2b9e32c6526a06874
oscar <oscarmtzz22@gmail.com>
2022-12-14 13:16:07 +0100

    modifica tiempos de intervalos para hacer la interfaz mas ligera

--- src/lib/estado-plc.js
+++ src/lib/estado-plc.js
@@ -715,7 +715,7 @@ timer(3000).then(async _ => {
     })
 });
 
-const milisegundos_actualizacion = 1500
+const milisegundos_actualizacion = 2500
 
 setInterval(async () => {
     if (variables.conectando) return
@@ -881,11 +881,21 @@ async function leer_segundos_ciclo(i) {
     variables.set_segundos_ciclo['valor_' + i] = (await s7.leer_int_db(variables.bloque_activo, variables.set_segundos_ciclo['posicion_' + i])).datos
 }
 
+let ciclo = 0
 async function leer_temperaturas(i) {
-    variables.temperatura_actual['valor_' + i] = Math.round((await s7.leer_real_db(variables.bloque_activo, variables.temperatura_actual['posicion_' + i])).datos * 100) / 100
-    variables.temperatura_actual_up['valor_' + i] = Math.round(((await s7.leer_real_db(variables.bloque_activo, variables.temperatura_actual_up['posicion_' + i])).datos / 10.0)  * 10)/ 10
-    variables.set_temperatura_up['valor_' + i] = (await s7.leer_real_db(variables.bloque_activo, variables.set_temperatura_up['posicion_' + i])).datos
-    variables.set_temperatura['valor_' + i] = (await s7.leer_real_db(variables.bloque_activo, variables.set_temperatura['posicion_' + i])).datos
+    ciclo += 1
+    if(ciclo % 2 === 0)
+    {
+        variables.temperatura_actual_up['valor_' + i] = Math.round(((await s7.leer_real_db(variables.bloque_activo, variables.temperatura_actual_up['posicion_' + i])).datos / 10.0)  * 10)/ 10
+        variables.set_temperatura_up['valor_' + i] = (await s7.leer_real_db(variables.bloque_activo, variables.set_temperatura_up['posicion_' + i])).datos
+    
+    }
+    else
+    {
+        variables.temperatura_actual['valor_' + i] = Math.round((await s7.leer_real_db(variables.bloque_activo, variables.temperatura_actual['posicion_' + i])).datos * 100) / 100
+        variables.set_temperatura['valor_' + i] = (await s7.leer_real_db(variables.bloque_activo, variables.set_temperatura['posicion_' + i])).datos
+    
+    }
     variables.temperatura_ok['valor_' + i] = (await s7.leer_bool_db(variables.bloque_activo, variables.temperatura_ok['posicion_' + i])).datos
 }
 
